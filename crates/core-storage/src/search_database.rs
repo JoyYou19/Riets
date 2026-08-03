39ffef57f5f33c855cba37e74e0be3bd990db08d
Krishinsh <kriistiiaans@gmail.com>
2026-08-03 14:58:05 +0300

    WAL biski strada

--- crates/core-storage/src/search_database.rs
+++ crates/core-storage/src/search_database.rs
@@ -23,6 +23,8 @@ use core_protocol::{
 };
 use core_query::{Query, QueryExecutor, SearchHit, planner::QueryPlan};
 use indexmap::IndexMap;
+use serde::{Serialize,Deserialize};
+use bincode::{Encode,Decode};
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum IndexMode {
     StoreOnly,
@@ -91,7 +93,7 @@ pub enum DatabasePowerButtonOutcome {
     Nochange,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Serialize,Deserialize,Clone,PartialEq,Encode,Decode)]
 pub struct DocumentInput {
     pub external_id: String,
     pub fields: BTreeMap<String, String>, //Don't know if this can be HashMap instead
