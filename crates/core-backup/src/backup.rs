8bedb24bf04ed54a0b25fde426f1603fcb34b0ef
Krishinsh <kriistiiaans@gmail.com>
2026-08-25 14:15:26 +0300

    fix

--- crates/core-backup/src/backup.rs
+++ crates/core-backup/src/backup.rs
@@ -413,23 +413,21 @@ impl BackupManager {
                 Err(_) => {
                     break;
                 }
-                Ok(m) =>
-                    match m.backup_type {
-                        BackupType::Full => {
-                            break;
+                Ok(m) => match m.backup_type {
+                    BackupType::Full => {
+                        break;
+                    }
+                    BackupType::Incremental => {
+                        let path = self.shard_backup_path(&current_id);
+                        if let Err(e) = fs::remove_dir_all(&path) {
+                            error!(
+                                self.log,
+                                "failed to delete old incremental {current_id}: {e}"
+                            );
                         }
-
-                        BackupType::Incremental => {
-                            let path = self.shard_backup_path(&current_id);
-                            if let Err(e) = fs::remove_dir_all(&path) {
-                                error!(
-                                    self.log,
-                                    "failed to delete old incremental {current_id}: {e}"
-                                );
-                            }
-                            None => {
-                                break;
-                            }
+                        match m.parent_backup_id {
+                            Some(parent_id) => current_id = parent_id,
+                            None => break,
                         }
                     }
                 },
